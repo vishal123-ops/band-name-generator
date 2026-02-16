23b50de3263ecc1446b8a767e6ff09e144dc830a
vishal123-ops <sirigiri.vishal2895@gmail.com>
2026-02-16 21:48:38 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome To The Band Name Generator!!")
+city = input("enter the city you grew up in:")
+pet = input("enter the pet name:")
+band_name= city + " " + pet
+print("your band name could be:",band_name)
